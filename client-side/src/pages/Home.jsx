748d2914e9eb300ad209085dc23a1d8edb644a64
dibpy13 <122885153+dibpy13@users.noreply.github.com>
2023-04-14 18:33:23 +0700

    updates2

--- client-side/src/pages/Home.jsx
+++ client-side/src/pages/Home.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 
 function Home() {
-const userList = useSelector((state) => state.users.user)
+const userGlobal = useSelector((state) => state.users.user)
 const dispatch = useDispatch()
 const user = userList
   return (
-    <div>Home</div>
+    <div>
+      <p>Welcome back, {userGlobal.name}</p>
+    </div>
   )
 }
 
